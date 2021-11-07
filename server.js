6959d5abda84a72e21dcdf72ea08f434f7a1f7c6
unknown <kingsleytwum2@gmail.com>
2021-11-07 19:59:01 +0000

    added body parser for the post request

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
+
+app.get('/twitter',(req,res)=>{
+res.sendFile(__dirname+"/index.html")
+})
 app.get('/',(req,res)=>{
-res.send('<h1>Welcome to Nodejs!</h1>')
+    res.sendFile(__dirname+"/form.html")
 })
 
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    res.send('posted')
+})
 app.listen(3000,()=>console.log('listening on port 3000'))
